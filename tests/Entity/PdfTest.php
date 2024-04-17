5ad94a25d6807cbf6b0ec7398530c83eb901e0d8
killian-dv <killian.david@etudiant.univ-reims.fr>
2024-04-17 14:16:21 +0200

    add unit tests

---
+++ tests/Entity/PdfTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\Pdf;
+use PHPUnit\Framework\TestCase;
+
+class PdfTest extends TestCase
+{
+    public function testGetterAndSetter()
+    {
+        // Création d'une instance de l'entité User
+        $pdf = new Pdf();
+
+        // Définition de données de test
+        $title = 'Test';
+        $createdAt = new \DateTimeImmutable();
+
+        // Utilisation des setters
+        $pdf->setTitle($title);
+        $pdf->setCreatedAt($createdAt);
+
+        // Vérification des getters
+        $this->assertEquals($title, $pdf->getTitle());
+        $this->assertEquals($createdAt, $pdf->getCreatedAt());
+
+    }
+}
